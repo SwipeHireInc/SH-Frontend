578b3eb42be689a9205d2a2884cffebf797bf05d
Asyl <duldievasylbek@gmail.com>
2025-05-31 16:59:47 +0500

    partstep

--- src/app/components/scroll-reveal-card/scroll-reveal-card.component.ts
+++ src/app/components/scroll-reveal-card/scroll-reveal-card.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -11,27 +11,23 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 export class ScrollRevealCardComponent implements AfterViewInit {
   @ViewChild('card', { static: true }) cardRef!: ElementRef;
 
-  constructor(private renderer: Renderer2) {}
-
   ngAfterViewInit() {
-    const card = this.cardRef.nativeElement;
+    gsap.registerPlugin(ScrollTrigger); 
 
-    // Заблокировать скролл
-    this.renderer.setStyle(document.body, 'overflow', 'hidden');
-
-    // Отслеживаем скролл
-    window.addEventListener('wheel', (e) => {
-      e.preventDefault();
-
-      // Двигаем карту вверх
-      gsap.to(card, {
-        y: '0%',
-        ease: 'power1.inOut',
-        duration: 1,
-        onComplete: () => {
-          this.renderer.removeStyle(document.body, 'overflow');
+    gsap.fromTo('.card', 
+      {
+         y: '-300%', 
+      },
+      { 
+        y: '-50%',
+        scrollTrigger: {
+          trigger: '.scroll-helper',
+          start: 'top bottom',
+          end: 'bottom bottom',
+          scrub: 1.5,
+          pin: '.portal-section'
         }
-      });
-    }, { passive: false });
+      }
+    );
   }
 }
