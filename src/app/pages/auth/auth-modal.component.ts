64ada6431a0dea8d692dbec0af88dd7cacebe23e
Asyl <duldievasylbek@gmail.com>
2025-06-06 23:43:12 +0500

    layoutform

--- src/app/pages/auth/auth-modal.component.ts
+++ src/app/pages/auth/auth-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild, AfterViewInit} from '@angular/core';
 import { animateOpen, animateClose } from './auth-modal.animation';
 import gsap from 'gsap';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-modal',
@@ -9,8 +10,18 @@ import gsap from 'gsap';
 })
 export class AuthModalComponent implements AfterViewInit {
   @Output() close = new EventEmitter<void>();
+  @Output() login = new EventEmitter<{username: string, password: string}>();
+  loginForm: FormGroup;
+  
   @ViewChild('modalOverlay', { static: false }) modalOverlay!: ElementRef;
   @ViewChild('modalContent', { static: false }) modalContent!: ElementRef;
+
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      
+    }
+    )
+  }
 
   closeModal() {
     animateClose(this.modalOverlay, this.modalContent, this.close);
