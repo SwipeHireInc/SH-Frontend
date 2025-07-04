aacd3f119ca9bafb68e52e38de47618816634d04
Asyl <duldievasylbek@gmail.com>
2025-07-04 23:04:48 +0500

    router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,18 @@ import { LandingComponent } from './pages/landing/landing.component';
 import { authGuard } from './core/guards/auth.guard';
 import { AuthCallbackComponent } from './core/services/AuthCallBack.component';
 import { MainComponent } from './pages/main/main.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 export const routes: Routes = [
     { path: 'landing', component: LandingComponent },
     { path: 'auth/callback', component: AuthCallbackComponent },
-    { path: 'main', component: MainComponent}, //canActivate: [authGuard]},
-    {
-        path: "app",
-        canActivate: [authGuard],
-        children: [
-            {path: 'swip', component: SwipComponent}
-            // {path: 'chat', component: ChatComponent},
-            // {path: 'profile', component: ProfileComponent},
-        ]
+    { path: 'main',
+      component: MainComponent,
+      children: [
+        {path: 'profile', component: ProfileComponent},
 
-    }
+      ]
+
+    }, //canActivate: [authGuard]},
 
 ];
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NavigationPanelComponent} from '../../shared/components/navigation-panel/navigation-panel.component';
+import {ProfileComponent} from '../profile/profile.component';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [CommonModule, NavigationPanelComponent],
+  imports: [CommonModule, NavigationPanelComponent, RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
