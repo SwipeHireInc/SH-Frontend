4bc6b961823b982ddbdd2d91d9359e82e5491203
Asyl <duldievasylbek@gmail.com>
2025-07-05 23:46:50 +0500

    goth-page

---
+++ src/app/pages/profile-page/company-page/company-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-page',
+  imports: [],
+  templateUrl: './company-page.component.html',
+  styleUrl: './company-page.component.scss'
+})
+export class CompanyPageComponent {
+
+}
